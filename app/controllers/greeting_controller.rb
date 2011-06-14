3082bcfadf47bc3c67dbef868dba24922b14f29d
Mike Piech <mpiech@engineyard.com>
2011-06-13 17:37:09 -0700

    first commit

---
+++ app/controllers/greeting_controller.rb
@@ -0,0 +1,6 @@
+class GreetingController < ApplicationController
+  def index
+    @welcome_message = "hellooooo world"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'df1f75a6ab940041e6e96de820eaf9ab57d54824bb55f9ad10c592c566d53c9de106ebcc65530c187bd322c55425d07dde539594be29a48537b76c13fdd94b94'
